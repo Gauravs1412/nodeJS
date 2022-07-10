ad6e119b665585a9b86767f936ad9e408939cb47
gaurav <gauravradadiya1412@gmail.com>
2022-07-10 11:54:00 +0530

    second commmit

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const http = require('http');
 
-const routes = require('./routs');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
 
 
 
-const server = http.createServer(routes);
+
+
+app.use('/add-product',(req,res,next) =>{
+    
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type = "submit">Add product</button></form>')
+})
+
+app.use('/product',(req,res,next) => {
+    console.log(req.body)
+    res.redirect('/');
+})
+
+
+app.use('/',(req,res,next) =>{
+    console.log("i am another middleware!")
+    res.send( 'hello from express')
+})
+
+
+
+
+
+const server = http.createServer(app);
 
  server.listen(1412);
