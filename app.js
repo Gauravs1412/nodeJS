c18dcee078fc5c933f8967b1a7073fe825fbddeb
gaurav <gauravradadiya1412@gmail.com>
2022-07-10 20:48:43 +0530

    3rd commit

--- app.js
+++ app.js
@@ -1,36 +1,21 @@
-const http = require('http');
+
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 
+app.use(bodyParser.urlencoded({extended:false}));  
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
+app.use((req,res,next) =>{
+    res.status(404).send('<h1>Page not found</h1>');
+});
 
-app.use('/add-product',(req,res,next) =>{
-    
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type = "submit">Add product</button></form>')
-})
-
-app.use('/product',(req,res,next) => {
-    console.log(req.body)
-    res.redirect('/');
-})
-
-
-app.use('/',(req,res,next) =>{
-    console.log("i am another middleware!")
-    res.send( 'hello from express')
-})
-
-
-
-
-
-const server = http.createServer(app);
-
- server.listen(1412);
+app.listen(1412);
